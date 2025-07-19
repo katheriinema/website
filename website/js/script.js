4ad1e94ef21cb81d3fcf0a60995234e82823eef0
kma <kathermaa@gmail.com>
2025-07-19 11:59:31 -0400

    Delete website directory

--- website/js/script.js
+++
@@ -1,9 +0,0 @@
-// Contact form handler
-const contactForm = document.getElementById('contactForm');
-if (contactForm) {
-  contactForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('感謝您的留言，我們將盡快與您聯絡！');
-    contactForm.reset();
-  });
-} 
